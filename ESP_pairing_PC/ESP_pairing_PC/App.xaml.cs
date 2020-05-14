e6322093e3377bc8dada7673417ef7ced5df64f6
Ergo <39900870+ergoLance@users.noreply.github.com>
2020-05-14 17:06:24 +0300

    added pc part

---
+++ ESP_pairing_PC/ESP_pairing_PC/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Drawing;
+
+namespace ESP_pairing_PC
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        System.Windows.Forms.NotifyIcon nIcon = new System.Windows.Forms.NotifyIcon();
+        
+        public App()
+        {
+            nIcon.Icon = new Icon(@"../../cat.ico");
+            nIcon.Visible = true;
+            nIcon.ShowBalloonTip(1000, "ergo", "ESP_pairing_PC", System.Windows.Forms.ToolTipIcon.Info);
+            nIcon.DoubleClick += nIcon_Click;
+        }
+
+        private void nIcon_Click(object sender, EventArgs e)
+        {
+            MainWindow.Visibility = Visibility.Visible;
+            MainWindow.WindowState = WindowState.Normal;
+        }
+    }
+}
